01b191e5a4dc15d4334898739d47f9906a6826c9
Abijith G Kaimal <abijithgkaimal@Abijiths-MacBook-Air.local>
2025-11-26 10:08:14 +0530

    latest updates

--- frontend/src/pages/Aboutpage.jsx
+++ frontend/src/pages/Aboutpage.jsx
@@ -16,30 +16,29 @@ import about3 from '../images/about3.png';
 function Aboutpage() {
   return (
     <>
-      <section id='about' className='py-5'>
+      <section id='about' className='p-4'>
         <Container fluid>
-          <Row className='mx-auto'>
+          <Row className=''>
 
-            <Col md={6} className='pt-md-5'>
-              <Row>
+            <Col md={6} className='pt-md-5 d-flex justify-content-center'>
+              <Row className='justify-content-center d-flex g-0' >
                 <Col xs={6}>
                   <Image src={about1} fluid alt="example" className='h-100' />
                 </Col>
 
                 <Col xs={6} className='d-flex flex-column'>
-                  <Image src={about2} fluid alt="example" className='w-75' />
-                  <Image src={about3} fluid alt="example" className='w-75 mt-2' />
+                  <Image src={about2} fluid alt="example" className='w-75 mx-auto' />
+                  <Image src={about3} fluid alt="example" className='w-75 mt-2 mx-auto' />
                 </Col>
               </Row>
             </Col>
 
 
             <Col md={6} className='text-md-start text-center pt-5'>
-              <h5>WHO WE ARE</h5>
+              <h5 id='ahead'>WHO WE ARE</h5>
               <h1 id='ahead1' className='my-4'>
                 Luxury Without the <br /> Lifetime Price Tag
               </h1>
-              <p className='mb-3' style={{fontStyle: 'italic', fontSize: '18px', color: '#0A3F36'}}>"Luxury Without the Lifetime Price Tag"</p>
 
               <p id='apara1' className='py-3'>
                 Zorucci is a luxury bridal wear brand based in Kerala, dedicated to curating exquisite bridal fashion. Since 2018, we’ve served thousands of brides with personalized styling, couture craftsmanship, and timeless bridal elegance. From traditional to modern, Zorucci combines heritage-rich details with contemporary design.
