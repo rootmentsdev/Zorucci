9edffe7af869ea390bedb650a001e193973cc6a3
Abijith G Kaimal <abijithgkaimal@Abijiths-MacBook-Air.local>
2025-12-05 14:36:23 +0530

    chore(zorucci-about-page): complete text and layout edits

--- frontend/src/pages/Aboutpage.jsx
+++ frontend/src/pages/Aboutpage.jsx
@@ -8,8 +8,7 @@ import '../styles/responsive.css'
 
 // About Section Images
 import about1 from '../images/about1.png';
-import about2 from '../images/about2.png';
-import about3 from '../images/about3.png';
+
 
 
 
@@ -20,31 +19,23 @@ function Aboutpage() {
         <Container fluid>
           <Row className=''>
 
-            <Col md={6} className='pt-md-2 d-flex justify-content-center'>
-              <Row className='justify-content-center d-flex g-0' >
-                <Col xs={6}>
-                  <Image src={about1} fluid alt="example" className='h-100' />
-                </Col>
+            <Col md={4} className=' py-3 d-flex justify-content-center'>
 
-                <Col xs={6} className='d-flex flex-column'>
-                  <Image src={about2} fluid alt="example" className='w-75 mx-auto' />
-                  <Image src={about3} fluid alt="example" className='w-75 mt-2 mx-auto' />
-                </Col>
-              </Row>
+            <Image src={about1} fluid alt="example" className='aboutimg1' style={{width:"374px" , height:"100%"} }/>
+        
             </Col>
 
-
-            <Col md={6} className='text-md-start text-center p-3'>
-              <h5 id='ahead'>WHO WE ARE</h5>
-              <h1 id='ahead1' className='my-4'>
+            <Col md={8} className='text-md-start text-center pe-md-5 mt-md-5'>
+              <h5 id='ahead' className='my-4'>WHO WE ARE</h5>
+              <h1 id='ahead1' className='my-md-4'>
                 Luxury Without the <br /> Lifetime Price Tag
               </h1>
 
-              <p id='apara1' className='py-3'>
+              <p id='apara1' className='py-4'>
                 Zorucci is a luxury bridal wear brand based in Kerala, dedicated to curating exquisite bridal fashion. Since 2018, we’ve served thousands of brides with personalized styling, couture craftsmanship, and timeless bridal elegance. From traditional to modern, Zorucci combines heritage-rich details with contemporary design.
               </p>
 
-              <p id='apara1' className='py-3'>
+              <p id='apara1' className=''>
                 With four showrooms across Kochi, Perinthalmanna, Kottakkal, and Edappal, we bring curated collections and expert stylists to brides across India.
               </p>
             </Col>
